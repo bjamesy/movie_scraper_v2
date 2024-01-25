8480755c7733baae37a93d5bae76d6e55066ae6b
james ballanger <jamesballanger@trentu.ca>
2024-01-25 12:25:06 -0500

    paradise scraper

--- app/router/v1/screenings.py
+++ app/router/v1/screenings.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter
 from src.scrapers.theatre_scraper import (
     get_revue,
-    get_tiff
+    # get_tiff,
+    # get_fox,
+    # get_carlton,
+    get_paradise
 )
 
 router = APIRouter()
 
 @router.get("/")
 async def get_screenings():
-    res = await get_tiff()
+    res = await get_paradise()
 
     return res
